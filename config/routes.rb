fb139ce7a3a9c4440eb98d2f13e16dd7926562d0
”shunyagit config --global user.name ”shunya <s.suzuki.spurs@gmail.com>
2019-05-18 15:57:45 +0000

    Ruby06課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+
   get 'sessions/new'
   root 'pages#index'
   get 'pages/help'
-  
-  resources :users
-  
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  
+  get    '/topics/index',   to: 'topics#index'
+  resources :topics
+  #resourcesだけだとtopics/indexの/indexが/:idと見なされてshowアクションに繋がってしまうのでその前にルーティングを設定
+
+
 end
