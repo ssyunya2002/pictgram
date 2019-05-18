f51817988f5f25a3526f424ee75acdef6d46aa1d
”shunyagit config --global user.name ”shunya <s.suzuki.spurs@gmail.com>
2019-05-18 04:46:42 +0000

    Ruby05課題

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   add_flash_types :success, :info, :warning, :danger
+  #「ApplicationControllerに「ある設定」を行い、ApplicationHelperのコードを削除できるようにしてください。」
+  #とのことだが、ApplicationHelperをIncludeをするのではダメなのか。逆だとうまくいかず、またググってもこの方法しか見つからず。。
+
+  include ApplicationHelper
+  
 end
