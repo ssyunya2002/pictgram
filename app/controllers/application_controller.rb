76c3b45fec7f273e7e989df3a5c1d9a0fbb5bdab
”shunyagit config --global user.name ”shunya <s.suzuki.spurs@gmail.com>
2019-05-19 02:43:34 +0000

    Rails05_rivisedv1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   #「ApplicationControllerに「ある設定」を行い、ApplicationHelperのコードを削除できるようにしてください。」
   #とのことだが、ApplicationHelperをIncludeをするのではダメなのか。逆だとうまくいかず、またググってもこの方法しか見つからず。。
+  helper_method :current_user, :logged_in?
 
-  include ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
   
 end
